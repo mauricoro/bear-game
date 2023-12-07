1a21ccb28c20bf21d9c8cc765f05220f97081161
mauricoro <119470826+mauricoro@users.noreply.github.com>
2023-12-06 21:54:53 -0600

    Test deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 // import VitePages from 'vite-plugin-pages'
-// import ViteLayouts from 'vite-plugin-vue-layouts'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/bear-game/',
+  base: '/bear-game',
 })
